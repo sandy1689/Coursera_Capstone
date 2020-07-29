29c4451188c61f9364d7cd1d99fe96b7e712b85e
Sandy <522735251@qq.com>
2020-07-29 09:52:20 +0800

    add import library

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,2 +1,6 @@
+#  this notebook will be mainly used for the capstone project.
+Import pandas as pd
+Import numpy as np
 
+print("Hello Capstone Project Course!")
 print("Hello Capstone Project Course!")
